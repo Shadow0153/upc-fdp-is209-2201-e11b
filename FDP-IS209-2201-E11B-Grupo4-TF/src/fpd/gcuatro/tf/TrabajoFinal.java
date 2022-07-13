5a476fa8cbb151952e469ac30c68a173df3fc64a
ANGELOHLH <angelohlh@gmail.com>
2022-07-12 21:42:13 -0500

    TEXTO DE INTEGRANTES DEL CURSO

--- FDP-IS209-2201-E11B-Grupo4-TF/src/fpd/gcuatro/tf/TrabajoFinal.java
+++ FDP-IS209-2201-E11B-Grupo4-TF/src/fpd/gcuatro/tf/TrabajoFinal.java
@@ -2,6 +2,16 @@ package fpd.gcuatro.tf;
 
 public class TrabajoFinal {
     public static void main (String[] args){
+        System.out.println("");
+        System.out.println("==================================================");
+        System.out.println("============FUNDAMENTOS DE PROGRAMACION===========");
+        System.out.println("=================TRABAJO FINAL====================");
+        System.out.println("==============CHRISTIAN CCASA ANCCANA=============");
+        System.out.println("==============ANGELO LEON HUAMANI=================");
+        System.out.println("==============VLADIMIR ANTAY RODAS================");
+        System.out.println("===============JOEL BARRANTES COPA================");
+        System.out.println("==================================================");
+        System.out.println("");
 
     }
 }
